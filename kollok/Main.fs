6bca3859c46bba32648b269c08811b40885c4f3b
Viacheslav <31708699+QuishKa@users.noreply.github.com>
2022-05-08 07:36:30 +0300

    input change

--- kollok/Main.fs
+++ kollok/Main.fs
@@ -1,6 +1,11 @@
 ﻿open System
+
 let input = 
     let x = Console.ReadLine().Split()
-    [for i in x -> Convert.ToInt32(i)]
-let x = example.add input
-printfn "%A" x
+    [for i in x -> 
+        try
+            Convert.ToInt32(i)
+        with
+            | :? System.Exception -> printfn $"Skipped value \"{i}\", not a number"; 0]
+
+printfn "%A" input
