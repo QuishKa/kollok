2c7ec1c9f144bea9ebdd0643299e731a9d4da648
pankoivan <81259100+pankoivan@users.noreply.github.com>
2022-05-08 12:44:20 +0300

    gcd

---
+++ kollok/Modules/gcd.fs
@@ -0,0 +1,20 @@
+﻿module gcd
+    open System
+    
+    let input = 
+        let xy = Console.ReadLine().Split()
+        [for i in xy -> 
+            try
+                Convert.ToInt32(i)
+            with
+                | :? System.Exception -> printfn $"This value is not an integer: {i}"; 0
+        ]
+    
+    let rec gcd x y = 
+        if y = 0 then x
+        else gcd y (x % y)
+    
+    let run = 
+        let xy = input
+        if xy.Length = 2 then Console.WriteLine(gcd xy[0] xy[1])
+        else Console.WriteLine("There must be two integers")
