a590a1b907711ecead65464973badeed70e717ea
Viacheslav <31708699+QuishKa@users.noreply.github.com>
2022-05-08 08:34:13 +0300

    ui with example

--- kollok/Main.fs
+++ kollok/Main.fs
@@ -1,6 +1,5 @@
 ﻿open System
-
-let input = 
+let input() = 
     let x = Console.ReadLine().Split()
     [for i in x -> 
         try
@@ -8,4 +7,22 @@ let input =
         with
             | :? System.Exception -> printfn $"Skipped value \"{i}\", not a number"; 0]
 
-printfn "%A" input
+let modules = ["example"]
+let mutable work = true
+while work do
+    Console.Clear()
+    printfn "Colloquium 2022 made by group 0308.\nTo run a module enter its number.\nEnter 0 to close program.\nModules:"
+    let mutable count = 0
+    for i in modules do
+        count <- count + 1
+        printfn "%d: %s" count i
+    let rec input() = 
+        try 
+            Convert.ToInt32(Console.ReadLine())
+        with
+            | :? System.Exception -> printfn "Enter a number."; input()
+    match input() with
+        | 0 -> work <- false
+        | 1 -> example.run;
+    printfn "The module finished. Press any key to continue..."
+    Console.ReadKey() |> ignore
--- kollok/Modules/example.fs
+++ kollok/Modules/example.fs
@@ -1,2 +1,18 @@
 ﻿module example
+    open System
+    let input() = 
+        let x = Console.ReadLine().Split()
+        [for i in x -> 
+            try
+                Convert.ToInt32(i)
+            with
+                | :? System.Exception -> printfn $"Skipped value \"{i}\", not a number"; 0]
     let add x = x |> List.map (fun q -> q + 1)
+    let run =
+        let mutable data = input()
+        match List.tryFindIndex(fun x -> x = 0) data with
+            | Some index -> data <- List.removeAt index data
+            | None -> None |> ignore
+        printfn "Entered array: %A" data
+        printfn $"{add data}"
+        
