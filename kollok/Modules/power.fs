08663e4dc05841a6997b48a6e0755a7e79585120
Viacheslav <31708699+QuishKa@users.noreply.github.com>
2022-05-09 20:45:11 +0300

    power fix

--- kollok/Modules/power.fs
+++ kollok/Modules/power.fs
@@ -1,7 +1,7 @@
 ﻿module power
     open System
 
-    let input = 
+    let input() = 
         let xn = Console.ReadLine().Split()
         [for i in xn -> 
             try
@@ -16,6 +16,6 @@
 
     let run() = 
         printfn "This module calculates the power of a number.\nEnter an integer and a natural number (including 0) to start."
-        let xn = input
+        let xn = input()
         if xn.Length = 2 && xn[1] >= 0 then Console.WriteLine(pow xn[0] xn[1])
         else Console.WriteLine("There must an integer and a natural number")
