1c1d8e5d71aa6758abf1d2f3f4ad8dd62bf958ce
botova-e <105191211+botova-e@users.noreply.github.com>
2022-05-10 09:34:39 +0300

    Add files via upload

---
+++ kollok/Modules/links_and_arrays.fs
@@ -0,0 +1,25 @@
+module links_and_arrays
+
+  let input = fun () ->
+    System.Convert.ToInt32(System.Console.ReadLine());
+
+  let run() = 
+    let x  = ref 6;
+    System.Console.WriteLine(  "Enter the integer number: ");
+    x := input();
+    printfn "The current value of the variable: %A" !x;
+    System.Console.WriteLine(  "Enter the integer you are going to change the value to: ");
+    x := input();
+    printfn "The current value of the variable: %A" !x;
+    x := !x + !x;
+    printfn "The current value of the variable (*2): %A" !x;
+    printfn "The current value of the variable without dereference: %A" x;
+
+    let array = [| for i in 1 .. 5 -> 0 |];
+    printfn "T: %A" array;
+    printfn "The value of the element at index 1: %d" (Array.get array 1);
+    Array.set array 1 10;
+    printfn "Replacing element at index 1 with 10: %A" array;
+    printf "The value of the element at index 1: %d " (Array.get array 1);
+    printfn "Press any key..."
+    x := System.Convert.ToInt32(System.Console.ReadLine());
